66a098ad11dd6173e221b2944c2f70c07d970c43
Fred Trotter <fred.trotter@gmail.com>
2013-09-29 07:37:03 -0500

    lets us bootstrap

---
+++ footer.php
@@ -0,0 +1,11 @@
+<?php
+
+echo '
+      <div class="footer">
+        <p>&copy; Fred Trotter and the DocGraph Journal 2013</p>
+      </div>
+    </div>
+    <!-- /container -->
+  </body>
+
+</html>';
