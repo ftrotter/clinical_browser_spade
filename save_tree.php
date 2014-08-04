31723cac22ba6dc8ded3faf246bca5d37d247f71
Fred Trotter <fred.trotter@gmail.com>
2014-08-04 18:52:06 -0500

    sql is in sourcecode

--- save_tree.php
+++ save_tree.php
@@ -1,22 +1,47 @@
 <?php
 	require_once('config.php');
+/*
+//The SQL for the logging table...
+
+CREATE TABLE IF NOT EXISTS `json_trees` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `tree_json` mediumtext NOT NULL,
+  `user_token` varchar(300) NOT NULL,
+  `upload_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+
+
+*/
+	
 
 	if(isset($_POST['url_tree'])){
 
-		$url_tree = mysql_real_escape_string($_POST['url_tree']);
-		$user_token = mysql_real_escape_string($_POST['user_token']);
+		$url_tree = $_POST['url_tree'];
+
+		if(!isJson($url_tree){
+			echo json_encode(array('result' => 'error', 'problem' => 'url_tree is not vaild json');
+			exit();
+
+		}
+
 
 		$url_tree_obj = json_decode($url_tree);
-		$url_tree_json_dump = var_export($url_tree_json,true);
+		$url_tree_json_dump = var_export($url_tree_obj,true);
 		$url_tree_dump = var_export($url_tree,true);
 
 		$url_string = "
 url_tree: $url_tree
 url_tree_dump: $url_tree_dump
-url_tree_obj: $url_tree_obj
 url_tree_json_dump: $url_tree_json_dump
 ";
 
+		$url_string = mysql_real_escape_string($url_string); //use this for testing...
+		
+		$url_string = mysql_real_escape_string($url_tree);
+
+		$user_token = mysql_real_escape_string($_POST['user_token']);
 		
 	$sql = "
 INSERT INTO  `browser_spade`.`json_trees` (
@@ -35,7 +60,7 @@ NULL
 
 		mysql_query($sql) or die("could not save sql $sql\n".mysql_error());
 
-		if(isset($_POST('echo')){
+		if(isset($_POST['echo'])){
 			if($_POST['echo']){			
 						
 				$url_tree_obj->result = 'saved';
@@ -59,7 +84,8 @@ url_tree:<br>
 <br>
 user_token:<br>
 <input name='user_token' type='text'><br>
-
+Echo back JSON in reply? <input type='checkbox' name='echo' value='true'>
+<br>
 <input type='submit'>
 </form>
 
@@ -69,6 +95,14 @@ user_token:<br>
 
 
 	}
+
+
+//thank you stack overflow...
+function isJson($string) {
+ json_decode($string);
+ return (json_last_error() == JSON_ERROR_NONE);
+}
+
 
 
 
